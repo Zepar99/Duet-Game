5d47cf4769c842880d219059bbf566ebb7bd49e7
Zepar99 <pratikkborse@gmail.com>
2022-04-20 23:57:34 +0530

    Final

--- Assets/Scripts/GameScripts/PauseController.cs
+++ Assets/Scripts/GameScripts/PauseController.cs
@@ -22,6 +22,6 @@ public class PauseController : MonoBehaviour
     public void Home(int sceneID)
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(0);
     }
 }
